090d438fd2e3bd338154cd718864adf3b0ba6257
lou-dupont <57115124+lou-dupont@users.noreply.github.com>
2020-03-08 00:27:06 +0100

    Simplifie la récupération de la liste des publiés
    inutile de boucler sur les 4 catégories car le moteur semble accepter de tout renvoyer d'un coup, cela allège le code

--- download.py
+++ download.py
@@ -23,38 +23,31 @@ if not os.path.exists(html_path):
 print('INFO: Downloading search result lists.')
 
 
-def build_payload(type_id, page):
-    return {'TYS_ID': type_id, 'BGS_NOM_ENTREPRISE': '', 'SEA_ID': '', 'RDR_CODE': '', 'BGS_ANNEE_REPORTING': '',
-            'MIN_SLR': '', 'MAX_SLR': '', 'IS_SCOPE3': 0, 'BT_ID': '', 'page': page}
+def build_payload(page):
+    return { 'page': page }
 
 url = 'http://www.bilans-ges.ademe.fr/fr/bilanenligne/bilans/xhr-page'
 published_indexes = []
 
 # To download the set of published indexes
-for type_id in [1, 2, 3, 4]:
-    response = requests.post(url, data=build_payload(type_id, 1))
+response = requests.post(url, data=build_payload(1))
+content = bs4.BeautifulSoup(response.content, 'lxml')
+count_text = content.find('h4', {'class': 'bilans'}).text
+count = int(re.sub('[^0-9]', '', count_text))
+print('DEBUG: Received %d results.' % (count))
+for page in range(count // 10 + 1):
+    print('DEBUG: Querying page %d.' % (page + 1))
+    response = requests.post(url, data=build_payload(page + 1))
     content = bs4.BeautifulSoup(response.content, 'lxml')
-    count_text = content.find('h4', {'class': 'bilans'}).text
-    count = int(re.sub('[^0-9]', '', count_text))
-    print('DEBUG: For type %d, received %d results.' % (type_id, count))
-    for page in range(count // 10 + 1):
-        print('DEBUG: Querying page %d.' % (page + 1))
-        response = requests.post(url, data=build_payload(type_id, page + 1))
-        content = bs4.BeautifulSoup(response.content, 'lxml')
-        links = content.find_all('a', {'class': 'button voir'})
-        for link in links:
-            report_id = re.sub('[^0-9]', '', link.get('href'))
-            published_indexes.append(int(report_id))
+    links = content.find_all('a', {'class': 'button voir'})
+    for link in links:
+        report_id = re.sub('[^0-9]', '', link.get('href'))
+        published_indexes.append(int(report_id))
 
 published_indexes = sorted(list(set(published_indexes)))
 with open(html_path + 'indexes.txt', 'w') as file:
     for index in published_indexes:
         file.write(str(index) + '\n')
-
-# To load the set of published indexes from a file
-# with open(html_path + 'indexes.txt', 'r') as file:
-    # for line in file:
-        # published_indexes.append(int(line.replace('\n', '')))
 
 print('INFO: Iterating over expected pages.')
 
